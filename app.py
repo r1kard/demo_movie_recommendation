806baa245094d6d47ff5f6676cb4509c2e1509e2
r1kard <riaguilar.c@gmail.com>
2024-07-29 21:05:34 -0400

    Comparision distrution similarity and percentile

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import requests
 import gzip
+import numpy as np
 
 APIKEY=st.secrets["APIKEY"]
 
@@ -19,16 +20,35 @@ def fetch_poster(movie_id):
     full_path= "https://image.tmdb.org/t/p/w500/"+poster_path
     return full_path
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key= lambda x: x[1])
+def recommend(movie, percentile=95):
+    idx = movies[movies['title'] == movie].index[0]
+    # Obtener las puntuaciones de similitud en coseno para todas las películas con esa película
+    sim_scores = list(enumerate(similarity[idx]))
+
+    # Extraer las puntuaciones de similitud
+    sim_values = [score[1] for score in sim_scores]
+
+    # Determinar el umbral basado en el percentil
+    threshold = np.percentile(sim_values, percentile)
+
+    # Filtrar las películas basadas en el umbral de similitud
+    sim_scores = [score for score in sim_scores if score[1] >= threshold]
+
+    # Ordenar las películas basadas en las puntuaciones de similitud
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
     recommended_movies_name = []
+
     recommendend_movies_poster = []
-    for i in distances[1:6]:
+
+    for i in sim_scores[1:6]:
         movie_id = movies.iloc[i[0]].movie_id
         recommendend_movies_poster.append(fetch_poster(movie_id))
         recommended_movies_name.append(movies.iloc[i[0]].title)
     return recommended_movies_name, recommendend_movies_poster
+
+
+    
 
 st.header("Sistema de Recomendacion de Películas con Machine Learning")
 with gzip.open('matrices/movie_list.pkl.gz', 'rb') as f:
